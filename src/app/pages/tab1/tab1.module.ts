84929836d3c61a9972816814a2329bf0ca0a0e12
crownclown501 <55864043+crownclown501@users.noreply.github.com>
2025-10-02 14:55:24 -0600

    Update tab1.module.ts

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { Tab1Page } from './tab1.page';
 import { ArticlesComponent } from '../../components/articles/articles.component'; // standalone
+import { FormsModule } from '@angular/forms';
+import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
+
 
 @NgModule({
   imports: [
-    CommonModule,
+   CommonModule,
     IonicModule,
-    ArticlesComponent, // standalone component importado aquí
-    Tab1Page // <-- aquí si es standalone
+     // standalone component importado aquí
+    Tab1Page, // <-- aquí si es standalone
+    Tab1PageRoutingModule,
+    FormsModule,
+    ExploreContainerComponentModule
   ]
   // No uses declarations si es standalone
 })
